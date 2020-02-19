e65b3e96573334a340ad6b6cc431b186b194c2b3
Nathan Fishel <nfishel@Nathans-MacBook-Air.local>
2020-02-19 14:22:45 -0500

    update

--- reader.java
+++ reader.java
@@ -1,3 +1,22 @@
-class reader{
+/*
+This class is used to read in text files and return them as 2D arrays
+to make use of data for the google hash code competition
+*/
+
+import java.io.IOException;  
+import java.io.RandomAccessFile;  
+import java.util.*;
+
+class Reader{
+    static RandomAccessFile raf;
+    static Scanner stdIn = new Scanner(System.in);
+
+    public static String getFileName(){
+        System.out.println("Please enter a file name\n> ");
+        String fileName = stdIn.nextLine();
+        return fileName;
+    }
+    public static void main(String args[]){
     
+    }
 }
